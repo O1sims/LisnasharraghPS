de2e0f7c772c124927a171d3c684d858f93e2ea0
Owen Sims <owen.sims@cyberlytic.com>
2018-04-25 12:20:01 +0100

    Adding website 'assets'

---
+++ assets/js/custom.js
@@ -0,0 +1,64 @@
+(function(){
+  var range = 250;
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop(),
+    height = $('#homeSplash').outerHeight(),
+    offset = height / 2,
+    calc = Math.max(1 - (scrollTop - offset + range) / range, 0.2);
+
+    $('#homeSplash').css({"opacity": calc });
+
+      if (calc > '1') {
+        $('#homeSplash').css({"opacity": 1});
+      } else if ( calc < '0.21' ) {
+        $('#homeSplash').css({"opacity": 0.2});
+      }
+  });
+
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
+
+  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+    var html = $(this).parent().html();
+    html = cleanSource(html);
+    $("#source-modal pre").text(html);
+    $("#source-modal").modal();
+  });
+
+  $(".bs-component").hover(function(){
+    $(this).append($button);
+    $button.show();
+  }, function(){
+    $button.hide();
+  });
+
+  function cleanSource(html) {
+    html = html.replace(/×/g, "&times;")
+               .replace(/«/g, "&laquo;")
+               .replace(/»/g, "&raquo;")
+               .replace(/←/g, "&larr;")
+               .replace(/→/g, "&rarr;");
+
+    var lines = html.split(/\n/);
+
+    lines.shift();
+    lines.splice(-1, 1);
+
+    var indentSize = lines[0].length - lines[0].trim().length,
+        re = new RegExp(" {" + indentSize + "}");
+
+    lines = lines.map(function(line){
+      if (line.match(re)) {
+        line = line.substring(indentSize);
+      }
+
+      return line;
+    });
+
+    lines = lines.join("\n");
+
+    return lines;
+  }
+
+})();
